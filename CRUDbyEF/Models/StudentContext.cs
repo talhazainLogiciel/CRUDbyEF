d33110c6484263dd476fed50c2e820cd6272a100
talhazainLogiciel <talha.zain@logicielservice.com>
2022-03-15 16:33:32 +0500

    Add project files.

---
+++ CRUDbyEF/Models/StudentContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDbyEF.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext()
+        {
+
+        }
+
+        public StudentContext(DbContextOptions<StudentContext>options):base (options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
